a7229a4806f77ce70cf2ace0aa81f205b8ac6521
Reid, Jon Carl <jreid2@hk220-mac1.local>
2018-10-15 15:39:39 -0700

    Initial Commit

---
+++ Meal Tracker FunTests/Meal_Tracker_FunTests.swift
@@ -0,0 +1,34 @@
+//
+//  Meal_Tracker_FunTests.swift
+//  Meal Tracker FunTests
+//
+//  Created by Reid, Jon Carl on 10/15/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import XCTest
+@testable import Meal_Tracker_Fun
+
+class Meal_Tracker_FunTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
